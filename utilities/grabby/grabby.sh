505b5a3390038a5a31e9ab15756ebaf588f08a94
phil.cryer <phil.cryer@02935b96-ba97-11de-81bc-59952d0011f9>
2010-01-18 03:05:33 +0000

    big update, checksums now work, it gens sha1 checksums after download, then compares those with the ones IA provides, gets all files by default, retries 3 times if there are network issues

--- utilities/grabby/grabby.sh
+++ utilities/grabby/grabby.sh
@@ -1,151 +1,113 @@
 #!/bin/bash
-#
-################################################################################
-#
-# File        	: grabby.sh
-# Usage		: ./grabby.sh 
-# Author      	: phil.cryer@mobot.org
-# Date created  : 2009-10-10
-# Last updated  : 2010-01-01
-# Source	: http://code.google.com/p/bhl-bits/utilities/grabby
-# Description 	: a bash script to perform batch downloads of Internet Archive
-#		  (archive.org) materials, via record ids as listed in todo.txt
-# Requires	: Bash, wget
-# (optional)    : fast/stable internet connection, paitience, sense of humor
-#
-################################################################################
-#
-# Copyright (c) 2010, Biodiversity Heritage Library
-#
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without 
-# modification, are permitted provided that the following conditions are met: 
-#
-# Redistributions of source code must retain the above copyright notice, this 
-# list of conditions and the following disclaimer. Redistributions in binary 
-# form must reproduce the above copyright notice, this list of conditions and the
-# following disclaimer in the documentation and/or other materials provided with
-# the distribution. Neither the name of the Biodiversity Heritage Library nor 
-# the names of its contributors may be used to endorse or promote products 
-# derived from this software without specific prior written permission. THIS
-# SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
-# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-################################################################################
-# More information about the BSD License can be found here:
-# http://www.opensource.org/licenses/bsd-license.php
-################################################################################
-#
+
 ########################################
-# Check todo, set time, make directory
+# Verify action file called 
 ########################################
-if [ ! -f "todo.txt" ]; then 
-	echo "can't find todo.txt, fail"
-	echo "define IA identifiers in todo.txt (one per line) and rerun"
-	exit 0
-fi 
-clear
-sum=0
-num=1
+if [ $# -ne 1 ]; then
+	echo "Usage: `basename $0` download_list"
+	exit 1
+fi
+if [ ! -f ${1} ]; then
+	echo "Fail: file ${1} not found"
+	exit 1
+fi
+
+########################################
+# Check for/create directories
+########################################
+if [ ! -d done ]; then 
+	mkdir done 
+fi
+if [ ! -d complete ]; then 
+	mkdir complete
+fi
+if [ ! -d failed ]; then 
+	mkdir failed
+fi
+
+########################################
+# Get report stats
+########################################
+sum=0; num=1; full=`cat ${1} | wc -l` 
 START_TIME=`date "+%H:%M:%S %Y-%m-%d%n"`
-echo "Starting download at ${START_TIME}" 
-echo "------------------------------------------------------"
-START=`date +%s`
-PUID=${2-${START}}
-COMPLETE_DIR=complete.${PUID}
-if [ ! -d ${COMPLETE_DIR} ]; then
-	mkdir ${COMPLETE_DIR}
-fi
-MANIFEST=00_manifest.${PUID}
-#
+PUID=`date +%s`
+
 ########################################
-# Inventory do/done downloads
+# Start the loop
 ########################################
-mv todo.txt  ${COMPLETE_DIR}
-cd ${COMPLETE_DIR}
-cat todo.txt | while read BOOK_ID
+cat ${1} | while read BOOK_ID
 do
-BASE_URL="http://archive.org/download/${BOOK_ID}"
 sum=$(($sum + $num))
-echo -n "$sum" > current.status.txt "of "
-TOTAL=`cat todo.txt | wc -l` 
-echo ${TOTAL} >> current.status.txt
-echo "title: ${BOOK_ID}" >> current.status.txt
-echo -n " [ `head -n1 current.status.txt` ]	Title: ${BOOK_ID}"; echo
-if [ -d "${BOOK_ID}" ]; then
-	echo "	- Existing data found, continuing previous download..."
-	if [ -f "${BOOK_ID}/index.html" ]; then 
-		rm ${BOOK_ID}/index.html 
-	fi 
-fi
-#
+
 ########################################
-# Build download list
+# Generate report file
 ########################################
-wget -p -c -nc -nH -nd -erobots=off -P${BOOK_ID} ${BASE_URL}
-grep "<a href=" ${BOOK_ID}/index.html | grep ${BOOK_ID} | grep -v "<h1" | cut -d">" -f1 | cut -d"\"" -f2 >> ${BOOK_ID}/xml_files_tmp
-cat ${BOOK_ID}/xml_files_tmp | sed s/^/http:\\/\\/archive.org\\/download\\/$BOOK_ID\\// > ${BOOK_ID}/download.urls
-rm ${BOOK_ID}/index.html; rm ${BOOK_ID}/xml_files_tmp*
-#
+TOTAL_DATA=`du -hc complete failed | tail -n1`
+TOTAL_COMPLETE=`ls complete/ | wc -l`
+TOTAL_FAILED=`ls failed/ | wc -l`
+START_TIME=`date "+%H:%M:%S %Y-%m-%d%n"`
+#END_TIME=`date "+%H:%M:%S %Y-%m-%d%n"`
+#START=`date +%s`
+#END=`date +%s`
+ELAPSED=`expr $END - $START`
+echo "<h3>grabby progress - running</h3>" > status
+echo "<ul>" >> status
+echo "<li>Process uid is ${PUID}</li>" >> status
+echo "<li>Running since ${START_TIME}</li>" >> status
+#echo "<li>Finished at ${END_TIME}</li>" >> status
+echo "<li>${TOTAL_COMPLETE} books downloaded successfully</li>" >> status
+echo "<li>${TOTAL_FAILED} books failed to download</li>" >> status
+#echo "<li>Download took ${ELAPSED} seconds</li>" >> status
+echo "<li>Total data downloaded `du -hc complete/ failed/ | tail -n1`</li>" >> status
+echo "</ul><hr>" >> status
+
 ########################################
 # Download files
 ########################################
-# download all related files (DEFAULT)
-wget -p -c -i ${BOOK_ID}/download.urls -nc -nH -nd -erobots=off -P${BOOK_ID} ${BASE_URL}
-# Notice: by default we now download every file related to the record id
-# if you want to limit this, manually grep out files here. in this example,
-# it will only djvu.txt files -  otherwise limit by file prefix on the wget line below
-#grep djvu.txt ${BOOK_ID}/download.urls > ${BOOK_ID}/download.urls-single
-#mv ${BOOK_ID}/download.urls-single ${BOOK_ID}/download.urls
-# or to limit downloads to only xml files
-#wget -p -c -A '.xml' -i ${BOOK_ID}/download.urls -nc -nH -nd -erobots=off -P${BOOK_ID} ${BASE_URL}
-#
+wget --user-agent="Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.2; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0)" --tries=2 --span-hosts --recursive --level=1 --continue --no-parent --no-host-directories --reject index.html --cut-dirs=2 --execute robots=off http://www.archive.org/download/${BOOK_ID} 
+	
 ########################################
-# Clean up download directory
+# Generate and check sha1 checksums
 ########################################
-rm ${BOOK_ID}/download.urls
-if [ -f "${BOOK_ID}/index.html" ]; then 
-	rm ${BOOK_ID}/index.html 
-fi 
-#mv ${BOOK_ID} ${COMPLETE_DIR}
-echo "Download of ${BOOK_ID} complete."
+echo; echo "    Generating SHA1 sums..."
+shasum ${BOOK_ID}/* > /tmp/shasums_${BOOK_ID} 
+echo; echo "    Checking SHA1 sums..."; echo 
+shasum -c /tmp/shasums_${BOOK_ID} > /tmp/cksums_${BOOK_ID}
+if [ `grep FAILED /tmp/cksums_${BOOK_ID} | wc -l` -gt '0' ]; then
+	mv ${BOOK_ID} failed
+	mv /tmp/cksums_${BOOK_ID} failed/${BOOK_ID}
+else
+	mv ${BOOK_ID} complete
+fi
+
+########################################
+# End loop, save download list to done
+########################################
 done
-#
+mv ${1} done/${PUID}.${1}
+
 ########################################
 # Summarize downloads, time, etc
 ########################################
-TOTAL_DATA=`du -hc | tail -n1`
-TOTAL_BOOKS=`cat current.status.txt | head -n1 | cut -d" " -f3`
+TOTAL_DATA=`du -hc complete failed | tail -n1`
+TOTAL_COMPLETE=`ls complete/ | wc -l`
+TOTAL_FAILED=`ls failed/ | wc -l`
+START_TIME=`date "+%H:%M:%S %Y-%m-%d%n"`
 END_TIME=`date "+%H:%M:%S %Y-%m-%d%n"`
-rm current.status.txt
-mv ${COMPLETE_DIR}/todo.txt ${COMPLETE_DIR}/00_download.${PUID}
-cd ..
-echo "------------------------------------------------------" > ${COMPLETE_DIR}/${MANIFEST}
-echo "Start time		${START_TIME}" >> ${COMPLETE_DIR}/${MANIFEST}
-echo "Finish time		${END_TIME} " >> ${COMPLETE_DIR}/${MANIFEST}
-echo "Data transfered		${TOTAL_DATA}" >> ${COMPLETE_DIR}/${MANIFEST}
-echo "Books transfered		${TOTAL_BOOKS}" >> ${COMPLETE_DIR}/${MANIFEST}
-echo "------------------------------------------------------" >> ${COMPLETE_DIR}/${MANIFEST}
-for f in $( ls ${COMPLETE_DIR} | grep -v ${MANIFEST} ); do echo $f >> ${COMPLETE_DIR}/${MANIFEST}; done
-echo "------------------------------------------------------"
-echo "Start time		${START_TIME}"
-echo "Finish time		${END_TIME} "
-echo "Data transfered		${TOTAL_DATA}"
-echo "Books transfered		${TOTAL_BOOKS}"
-echo "------------------------------------------------------"
-FINISH=`date +%s`
-ELAPSED=`expr $FINISH - $START`
-echo "------------------------------------------------------" >> ${COMPLETE_DIR}/${MANIFEST}
-echo "Total download time: ${ELAPSED} seconds"
-echo "Total download time: ${ELAPSED} seconds" >> ${COMPLETE_DIR}/${MANIFEST}
-echo "Files downloaded to: ${COMPLETE_DIR}"
+START=`date +%s`
+END=`date +%s`
+ELAPSED=`expr $END - $START`
+echo "<h3>grabby progress - completed</h3>" > status
+echo "<ul>" >> status
+echo "<li>Process uid was ${PUID}</li>" >> status
+echo "<li>Started at ${START_TIME}</li>" >> status
+echo "<li>Finished at ${END_TIME}</li>" >> status
+echo "<li>${TOTAL_COMPLETE} books downloaded successfully</li>" >> status
+echo "<li>${TOTAL_FAILED} books failed to download</li>" >> status
+echo "<li>Download took ${ELAPSED} seconds</li>" >> status
+echo "<li>Total data downloaded `du -hc complete/ failed/ | tail -n1`</li>" >> status
+echo "</ul><hr>" >> status
+cp status done/${PUID}.status
+
 exit 0
+
