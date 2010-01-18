7ce29d855d6e41416a2d84f70dbfbf203849c999
phil.cryer <phil.cryer@02935b96-ba97-11de-81bc-59952d0011f9>
2010-01-18 03:06:35 +0000

    fixed some of the verbage in the top of the script

--- trunk/utilities/grabby/grabby.sh
+++ trunk/utilities/grabby/grabby.sh
@@ -1,5 +1,50 @@
 #!/bin/bash
-
+#
+################################################################################
+#
+# File          : grabby.sh
+# Usage         : ./grabby.sh
+# Author        : phil.cryer@mobot.org
+# Date created  : 2009-10-10
+# Last updated  : 2010-01-17
+# Source        : http://code.google.com/p/bhl-bits/utilities/grabby
+# Description   : a bash script to perform batch downloads of Internet Archive
+#                 (archive.org) materials, via record ids as listed in todo.txt
+# Requires      : Bash, wget
+# (optional)    : fast/stable internet connection, paitience, sense of humor
+#
+################################################################################
+#
+# Copyright (c) 2010, Biodiversity Heritage Library
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer. Redistributions in binary
+# form must reproduce the above copyright notice, this list of conditions and the
+# following disclaimer in the documentation and/or other materials provided with
+# the distribution. Neither the name of the Biodiversity Heritage Library nor
+# the names of its contributors may be used to endorse or promote products
+# derived from this software without specific prior written permission. THIS
+# SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+################################################################################
+# More information about the BSD License can be found here:
+# http://www.opensource.org/licenses/bsd-license.php
+################################################################################
+#
 ########################################
 # Verify action file called 
 ########################################
