5d3c5df3a9bc32f48710c9533b2a2b0c4e9f2c75
phil.cryer <phil.cryer@02935b96-ba97-11de-81bc-59952d0011f9>
2010-01-20 04:55:14 +0000

    fixed sha1 check, it gens a hash from our dnld file, then greps for it in _files.xml

--- trunk/utilities/grabby/extras/sumthing_sha1.sh
+++ trunk/utilities/grabby/extras/sumthing_sha1.sh
@@ -43,38 +43,65 @@ echo " Sumthing_sha1 starting"
 echo "------------------------"
 echo
 
-#####
+######################################################################
 cat /tmp/sumbook_list | while read BOOK_ID
 do
 echo "	-> Checking ${BOOK_ID} sha1 sums"
 
-##########
+##################################################
 ls -1 ${1}/${BOOK_ID} > /tmp/sumfile_list
 cat /tmp/sumfile_list | while read FILE_ID
 do
 
-echo -n "		* checking file ${FILE_ID}..."
+#echo -n "		* `echo ${FILE_ID} | cut -d"." -f2`"
+echo -n "		* `echo ${FILE_ID} | cut -d"." -f2`"
+#echo -n "		* ${FILE_ID}"
+#echo $FILE_ID
 
 if [ ! -f ${1}/${BOOK_ID}/${BOOK_ID}_files.xml ]; then
 	echo "		* FAIL! can't find file ${1}/${BOOK_ID}/${BOOK_ID}_files.xml"
 fi
 
-grep_output="$(grep `sha1sum ${1}/${BOOK_ID}/${FILE_ID}` ${1}/${BOOK_ID}/${BOOK_ID}_files.xml)"
+OUR_SUMS=`sha1sum ${1}/${BOOK_ID}/${FILE_ID}`
+OUR_SUM=`echo ${OUR_SUMS} | cut -d" " -f1`
+#echo $OUR_SUM
+
+#echo ${OUR_SUM} 
+#echo ${1}/${BOOK_ID}/${FILE_ID}
+#echo ${1}/${BOOK_ID}/${BOOK_ID}_files.xml
+
+#grep_output="$(grep `sha1sum ${1}/${BOOK_ID}/${FILE_ID}` ${1}/${BOOK_ID}/${BOOK_ID}_files.xml)"
+#grep_output="$(grep -i `sha1sum ${1}/${BOOK_ID}/${FILE_ID}` ${1}/${BOOK_ID}/${BOOK_ID}_files.xml)"
+grep_output="$(grep ${OUR_SUM} ${1}/${BOOK_ID}/${BOOK_ID}_files.xml)"
+echo -n "			  ${OUR_SUM} ..............."
+#echo "			IA  ${OUR_SUM}"
+
+#exit 0
+
+#grep_output="$(grep ${OUR_SUM} ${1}/${BOOK_ID}/${BOOK_ID}_files.xml)"
+
+#echo `cat $grep_output | cut -d" " -f1`
+
+#exit 0
 
 if [ -z "$grep_output" ]; then
-	echo "FAIL!" 
-	echo "		* file ${FILE_ID} has failed the sha1 checksum, a note has been made in sumthing_sha1-${DATE}/fail"
+	echo " [ FAIL! ]" 
+	#echo "		* file ${FILE_ID} has failed the sha1 checksum, a note has been made in sumthing_sha1-${DATE}/fail"
 	echo ${BOOK_ID}/${FILE_ID} >> sumthing_sha1-${DATE}/fail
 else
-	echo "OK" 
+	echo " [ OK ]" 
 	echo ${BOOK_ID}/${FILE_ID} >> sumthing_sha1-${DATE}/pass 
 fi
 
 done
-##########
+				       #
+				       #
+########################################
 
 done
-#####
+				                                                #
+				                                                #
+#################################################################################
 
 rm /tmp/sumbook_list
 rm /tmp/sumfile_list
