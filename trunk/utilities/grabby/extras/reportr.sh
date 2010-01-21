a824e7e31fa1e9c68f6899551f366d9533ec988a
phil.cryer <phil.cryer@02935b96-ba97-11de-81bc-59952d0011f9>
2010-01-21 20:59:32 +0000

    updated

--- trunk/utilities/grabby/extras/reportr.sh
+++ trunk/utilities/grabby/extras/reportr.sh
@@ -1,7 +1,12 @@
 cd /root
 
-echo "<h2><div align="center">IA download status as of `date`</h2>" > index.html
-echo "<div align="center">(updated every 10 minutes)</div>" >> index.html
+echo "<html><head><title>grabby status</title></head><body>" > index.html
+echo "<h2><div align="center">IA download status as of `date`</h2>" >> index.html
+echo "<div align="center">" >> index.html
+echo -n "Current download rate "`bwm-ng -I eth1 -o html -T rate | cut -d":" -f3 | cut -d"<" -f4 | cut -d">" -f2` >> index.html
+#echo -n " - total downloaded "`du -hc /mnt/export/grabby*|tail -n1|cut -f1` >> index.html
+echo -n " - total downloaded `df -h | tail -n1 | cut -d"T" -f2` Terabytes" >> index.html
+echo " - updated every 10 minutes</div>" >> index.html
 echo "<hr>" >> index.html
 #
 
@@ -9,15 +14,16 @@ echo "<hr>" >> index.html
 cat /mnt/export/grabby*/status >> index.html
 
 #
-echo "Total download rate across all processes "`bwm-ng -I eth1 -o html -T rate | cut -d":" -f3 | cut -d"<" -f4 | cut -d">" -f2` >> index.html
-echo "<br />" >> index.html
+echo "<div align="center"><a href="http://www.youtube.com/watch?v=QvqDYj3mkGk" alt="Dexter\'s Laboratory"><img src="dexter.jpg" border="0"></a>" >> index.html
+echo "<br /><br />" >> index.html 
+echo "<hr>" >> index.html
 #
-echo "Total data downloaded across all processes "`du -hc /mnt/export/grabby*|tail -n1|cut -f1` >> index.html
-#
-echo "<br /><br />Browse the downloaded files <a href="http://whbhl01.ubio.org/files/">here</a>" >> index.html
-#
-#
-echo "<br /><div align="center"><a href="http://www.youtube.com/watch?v=QvqDYj3mkGk" alt="Dexter\'s Laboratory"><img src="dexter.jpg" border="0"></a></div>" >> index.html
+echo -n "<a href="http://files.biodiversitylibrary.org/">files</a>  -  " >> index.html
+echo -n "<a href="http://cluster.biodiversitylibrary.org/dir/">directories</a>  -  " >> index.html
+echo "<a href="http://www.slideshare.net/phil.cryer/building-a-scalable-open-source-storage-solution-2482448">about</a></div>" >> index.html
+
+echo "</body></html>" >> index.html
 chown www-data:www-data index.html
-mv index.html /var/www/cluster
+cp -p index.html /var/www/cluster
+mv index.html /var/www/cluster.biodiversitylibrary.org
 exit 0
