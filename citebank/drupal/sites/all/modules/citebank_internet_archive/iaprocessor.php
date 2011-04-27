fa2486cb453b3d127144b654d3b70930bc78ec1d
dheskettmobot <dheskettmobot@gmail.com>
2011-04-27 19:34:50 +0000

    Adding external cron modules

---
+++ citebank/drupal/sites/all/modules/citebank_internet_archive/iaprocessor.php
@@ -0,0 +1,40 @@
+<?php
+
+// Internet Archive cron processor
+
+// $Id: iaprocessor.php,v 1.0.0.0 2011/04/19 4:44:44 dlheskett $
+
+/**  iaprocessor class
+ *
+ * Copyright (c) 2011 Missouri Botanical Garden 
+ *
+ * @author: David L. Heskett (contractor: Adaptive Solutions Group)
+ * @date Created: 04/19/2011
+ *
+ */
+
+$includePath = dirname(__FILE__) . '/';
+
+require_once($includePath . 'CronIAProcessor.php');
+
+// ****************************************
+// ****************************************
+// ****************************************
+echo "\n";
+echo 'start' . date('Y/m/d H:i:s');
+echo "\n";
+$cronIA = new CronIAProcessor();
+echo "\n";
+echo 'begin' . date('Y/m/d H:i:s');
+echo "\n";
+$cronIA->runInternetArchiveCron();
+
+echo "\n";
+echo 'end' . date('Y/m/d H:i:s');
+echo "\n";
+
+// ****************************************
+// ****************************************
+// ****************************************
+
+?>
