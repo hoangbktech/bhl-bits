bd15cb9ca400541887ae43827ac1b2d781f8dfce
sprogerb@ait.co.at <sprogerb@ait.co.at@02935b96-ba97-11de-81bc-59952d0011f9>
2011-07-05 14:11:24 +0000

    converted jdom to w3c dom, formatted xerces output

--- trunk/bhl-e/pre-ingest/PreIngest/src/main/java/at/co/ait/domain/services/MetsMarshallerService.java
+++ trunk/bhl-e/pre-ingest/PreIngest/src/main/java/at/co/ait/domain/services/MetsMarshallerService.java
@@ -4,22 +4,16 @@ import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.List;
 import java.util.TimeZone;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.apache.commons.io.FileUtils;
 import org.jdom.JDOMException;
 import org.jdom.input.SAXBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.jdom.output.DOMOutputter;
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
 
 import at.co.ait.domain.oais.DigitalObject;
@@ -39,49 +33,31 @@ import au.edu.apsr.mtk.base.MdWrap;
 import au.edu.apsr.mtk.base.MetsHdr;
 import au.edu.apsr.mtk.base.StructMap;
 
+import com.sun.org.apache.xml.internal.serialize.OutputFormat;
+import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
+
 public class MetsMarshallerService {
-	
-	private List<DigitalObject> packagedFiles = new ArrayList<DigitalObject>();
-	private static final Logger logger = LoggerFactory.getLogger(MetsMarshallerService.class);
+
 	private METS mets = null;
-	
-    static private Document createMODS(String title, String genre) throws ParserConfigurationException
-    {
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder builder = factory.newDocumentBuilder();
-        Document doc = builder.newDocument();
-        Element root = doc.createElementNS("http://www.loc.gov/mods/v3", "mods");
-        doc.appendChild(root);
-        
-        Element ti = doc.createElement("titleInfo");
-        Element t = doc.createElement("title");
-        t.setTextContent(title);
-        ti.appendChild(t);
-        root.appendChild(ti);
-        
-        Element g = doc.createElement("genre");
-        g.setTextContent(genre);
-        root.appendChild(g);
-        
-        return doc;
-    }
-    
-    private METSWrapper createMETS(InformationPackageObject obj) throws JDOMException, ParserConfigurationException {
-    	METSWrapper mw = null;
-    	try {
+
+	private METSWrapper createMETS(InformationPackageObject obj)
+			throws JDOMException, ParserConfigurationException {
+		METSWrapper mw = null;
+		try {
 			mw = new METSWrapper();
-			
-	        mets = mw.getMETSObject();
-	       
-	        mets.setObjID(String.valueOf(obj.getIdentifier()));
+
+			mets = mw.getMETSObject();
+
+			mets.setObjID(String.valueOf(obj.getIdentifier()));
 			mets.setProfile("http://www.bhl-europe.eu/profiles/bhle-mets-profile-1.0");
 			mets.setType("investigation");
-			
+
 			MetsHdr mh = mets.newMetsHdr();
-			
-	    	SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
-	    	Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT+1"));
-	    	String currentTime = df.format(cal.getTime());
+
+			SimpleDateFormat df = new SimpleDateFormat(
+					"yyyy-MM-dd'T'HH:mm:ss'Z'");
+			Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT+1"));
+			String currentTime = df.format(cal.getTime());
 			mh.setCreateDate(currentTime);
 			mh.setLastModDate(currentTime);
 
@@ -89,60 +65,60 @@ public class MetsMarshallerService {
 			agent.setRole("CREATOR");
 			agent.setType("OTHER");
 			agent.setName("SampleMETSBuild");
-			
+
 			mh.addAgent(agent);
 			mets.setMetsHdr(mh);
-			
+
 			DmdSec dmd = mets.newDmdSec();
 			dmd.setID("J-1");
 			MdWrap mdw = dmd.newMdWrap();
 			mdw.setMDType("MODS");
-//			SAXBuilder parser = new SAXBuilder();
-//			Document modsdoc = null;
-//			for (DigitalObject digobj : obj.getDigitalobjects()) {
-//				if (digobj.getSmtoutput() != null) {
-//					System.out.println("building mods");
-//					modsdoc = (Document) parser.build(digobj.getSmtoutput()); 
-//				}
-//			}			
-//			mdw.setXmlData(modsdoc.getDocumentElement());
-			mdw.setXmlData(createMODS("demo", "experiment").getDocumentElement());
+
+			SAXBuilder parser = new SAXBuilder();
+			org.jdom.Document modsdoc = null;
+			for (DigitalObject digobj : obj.getDigitalobjects()) {
+				if (digobj.getSmtoutput() != null) {
+					System.out.println("building mods");
+					modsdoc = parser.build(digobj.getSmtoutput());
+				}
+			}
+			DOMOutputter domOutputter = new DOMOutputter();
+			org.w3c.dom.Document w3cDoc = domOutputter.output(modsdoc);
+			mdw.setXmlData(w3cDoc.getDocumentElement());
 			dmd.setMdWrap(mdw);
-			
+
 			mets.addDmdSec(dmd);
 			FileSec fs = mets.newFileSec();
 			FileGrp fg = fs.newFileGrp();
 			int iter = 0;
-			
-			synchronized(obj.getDigitalobjects()) {
-				for (DigitalObject digobj : obj.getDigitalobjects()) {	
-					
-					fg.setUse("original");
-					
-					au.edu.apsr.mtk.base.File f = fg.newFile();
-					f.setID("F-"+(++iter));				
-					f.setSize(FileUtils.sizeOf(digobj.getSubmittedFile()));
-					f.setMIMEType("application/octet-stream");
-					f.setOwnerID("at.co.ait");
-					f.setChecksum(digobj.getDigestValueinHex());
-					f.setChecksumType("SHA-1");				
-					
-					FLocat fl = f.newFLocat();				
-					fl.setHref(digobj.getSubmittedFile().getAbsolutePath());
-					fl.setLocType("URL");
-					
-					f.addFLocat(fl);
-					fg.addFile(f);
-						
-				}
+
+			for (DigitalObject digobj : obj.getDigitalobjects()) {
+
+				fg.setUse("original");
+
+				au.edu.apsr.mtk.base.File f = fg.newFile();
+				f.setID("F-" + (++iter));
+				f.setSize(FileUtils.sizeOf(digobj.getSubmittedFile()));
+				f.setMIMEType("application/octet-stream");
+				f.setOwnerID("at.co.ait");
+				f.setChecksum(digobj.getDigestValueinHex());
+				f.setChecksumType("SHA-1");
+
+				FLocat fl = f.newFLocat();
+				fl.setHref(digobj.getSubmittedFile().getAbsolutePath());
+				fl.setLocType("URL");
+
+				f.addFLocat(fl);
+				fg.addFile(f);
+
 			}
-			
+
 			fs.addFileGrp(fg);
 			mets.setFileSec(fs);
-			
+
 			StructMap sm = mets.newStructMap();
 			mets.addStructMap(sm);
-			
+
 			Div d = sm.newDiv();
 			d.setType("investigation");
 			d.setDmdID("J-1");
@@ -153,7 +129,7 @@ public class MetsMarshallerService {
 			d.addFptr(fp);
 
 			mw.validate();
-			
+
 		} catch (METSException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -167,26 +143,32 @@ public class MetsMarshallerService {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    	return mw;
-    }
-    
-    /**
-     * Enriches the information package object by adding METS metadata
-     * @param obj
-     * @return
-     * @throws ParserConfigurationException 
-     * @throws JDOMException 
-     * @throws IOException 
-     */
-    public InformationPackageObject marshal(InformationPackageObject obj) throws JDOMException, ParserConfigurationException, IOException {
-    	System.out.println("marshalling to METS");
-    	obj.setMets(createMETS(obj));    	
-		String tmpfile = ConfigUtils.getTmpFileName(obj.getSubmittedFile(),".mets.xml");
-		System.out.println("saving mets to " + tmpfile);
+		return mw;
+	}
+
+	/**
+	 * Enriches the information package object by adding METS metadata
+	 * 
+	 * @param obj
+	 * @return
+	 * @throws ParserConfigurationException
+	 * @throws JDOMException
+	 * @throws IOException
+	 */
+	public InformationPackageObject marshal(InformationPackageObject obj)
+			throws JDOMException, ParserConfigurationException, IOException {
+		obj.setMets(createMETS(obj));
+		String tmpfile = ConfigUtils.getTmpFileName(obj.getSubmittedFile(),
+				".mets.xml");
 		File metsfile = new File(tmpfile);
-		obj.getMets().write(FileUtils.openOutputStream(metsfile));    	
-    	return obj;
-    }
-		
-	
+		Document doc = obj.getMets().getMETSDocument();
+		OutputFormat format = new OutputFormat(doc);
+		format.setLineWidth(65);
+		format.setIndenting(true);
+		format.setIndent(2);
+		XMLSerializer serializer = new XMLSerializer(FileUtils.openOutputStream(metsfile), format);
+		serializer.serialize(doc);
+		return obj;
+	}
+
 }
