72ef73579755eb52c9993e8907d51d0f3c8f8eb6
bendao <bendao@ait.co.at>
2011-08-11 09:11:34 +0000

    fix: make OCRSerivce immutable for parallel execution

--- bhl-e/pre-ingest/PreIngest/src/main/java/at/co/ait/domain/services/OCRService.java
+++ bhl-e/pre-ingest/PreIngest/src/main/java/at/co/ait/domain/services/OCRService.java
@@ -7,22 +7,18 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import at.co.ait.domain.oais.DigitalObject;
 import at.co.ait.utils.ConfigUtils;
 import at.co.ait.utils.Configuration;
-import at.co.ait.web.common.UserPreferences;
+
 
 public class OCRService extends ProcessbuilderService {
 
 	private static final Logger logger = LoggerFactory
 			.getLogger(OCRService.class);
-
-	private List<String> commands;
-	private UserPreferences prefs;
 
 	public DigitalObject scan(DigitalObject obj, String lang) throws MalformedURLException,
 			IOException, InterruptedException {		
@@ -30,7 +26,7 @@ public class OCRService extends ProcessbuilderService {
 		String tmpfile = ConfigUtils.getTmpFileName(obj.getSubmittedFile(),Configuration.getString("OCRService.postfix")); //$NON-NLS-1$
 		String srcfile = obj.getSubmittedFile().getAbsolutePath();		
 		URL ocrexec = new URL(Configuration.getString("OCRService.executable")); //$NON-NLS-1$		
-		commands = new ArrayList<String>();
+		List<String> commands = new ArrayList<String>();
 		commands.add(ocrexec.getPath());
 		commands.add(srcfile);
 		commands.add(tmpfile);
