1645f03143a944ee988df2eca86ebbb0cf0c4476
bendao <bendao@ait.co.at>
2011-08-11 09:12:33 +0000

    log command execution for debugging invalid config setting

--- bhl-e/pre-ingest/PreIngest/src/main/java/at/co/ait/domain/services/ProcessbuilderService.java
+++ bhl-e/pre-ingest/PreIngest/src/main/java/at/co/ait/domain/services/ProcessbuilderService.java
@@ -1,12 +1,18 @@
 package at.co.ait.domain.services;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public abstract class ProcessbuilderService {
 	// can run basic ls or ps commands
 	// can run command pipelines
 	// can run sudo command if you know the password is correct
+	private static final Logger logger = LoggerFactory
+		.getLogger(ProcessbuilderService.class);
 	
 	int result;
 	String stdout;
@@ -26,6 +32,9 @@ public abstract class ProcessbuilderService {
 
 	public void process(List<String> commands) throws IOException, InterruptedException {
 		// execute the command
+		if(logger.isDebugEnabled()) {
+			logger.debug("exec: " + Arrays.toString(commands.toArray()));
+		}
 		SystemCommandExecutor commandExecutor = new SystemCommandExecutor(
 				commands);
 		result = commandExecutor.executeCommand();
