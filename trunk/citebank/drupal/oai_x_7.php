5419f67a2add27a97119dc18a9ef757a7aa2782e
dheskettmobot@gmail.com <dheskettmobot@gmail.com@02935b96-ba97-11de-81bc-59952d0011f9>
2011-11-23 15:28:13 +0000

    updated

--- trunk/citebank/drupal/oai_x_7.php
+++ trunk/citebank/drupal/oai_x_7.php
@@ -18,35 +18,35 @@ $harvestId = 7;
 
 $includePath = dirname(__FILE__) . '/';
 
-require_once($includePath . 'manualOaiHarvester.php');
-return;
-//
-//require_once($includePath . 'manualOaiHarvesterModel.php');
-//
-//// prevent crons colliding, there can be only one highlander
-//$nameHarvestRunning = 'harvest_running_' . $harvestId;
-//
-//$flagHarvestRunning = variable_get($nameHarvestRunning, false);
-//
-//if ($flagHarvestRunning) {
-//	return;
-//}
-//variable_set($nameHarvestRunning, 1);
-//
-//$x =  new manualOaiHarvesterModel();
-//$msg = 'harvest id:' . $harvestId;
-//$x->myOaiLogMsg($msg);
-//
-//$msg = 'Start:' . date('YmdHis');
-//$x->myOaiLogMsg($msg);
-//
-//// get started
-//$x->manualOaiHarvesterCron($harvestId);
-//
-//$msg = 'End:' . date('YmdHis');
-//$x->myOaiLogMsg($msg);
-//
-//variable_del($nameHarvestRunning);
+//require_once($includePath . 'manualOaiHarvester.php');
+//return;
+
+require_once($includePath . 'manualOaiHarvesterModel.php');
+
+// prevent crons colliding, there can be only one highlander
+$nameHarvestRunning = 'harvest_running_' . $harvestId;
+
+$flagHarvestRunning = variable_get($nameHarvestRunning, false);
+
+if ($flagHarvestRunning) {
+	return;
+}
+variable_set($nameHarvestRunning, 1);
+
+$x =  new manualOaiHarvesterModel();
+$msg = 'harvest id:' . $harvestId;
+$x->myOaiLogMsg($msg);
+
+$msg = 'Start:' . date('YmdHis');
+$x->myOaiLogMsg($msg);
+
+// get started
+$x->manualOaiHarvesterCron($harvestId);
+
+$msg = 'End:' . date('YmdHis');
+$x->myOaiLogMsg($msg);
+
+variable_del($nameHarvestRunning);
 
 // **************************************************
 ?>
